5d3ccc7bdffdc9247c7db2ec9e924784cf2d9503
Leonhard Siegele <leonhard.siegele39@gmail.com>
2026-06-15 21:00:57 +0200

    setUp newSurvey From

--- src/app/components/create-survey-component/create-survey-component.ts
+++ src/app/components/create-survey-component/create-survey-component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+
 @Component({
   selector: 'app-create-survey-component',
   imports: [],
@@ -9,5 +10,19 @@ import { RouterLink } from '@angular/router';
 })
 export class CreateSurveyComponent {
 
+  // openDropDown(){
+  //   let drop = document.querySelector('.dropDownList') as HTMLDivElement
+  //   drop.style.transition = 'all 0.5s ease-in-out';
+  //   drop.style.opacity = '1';
+  //   drop.style.height = '300px';
+  // }
+
+
+  isDropdownOpen = false;
+
+
+toggleDropDown() {
+  this.isDropdownOpen = !this.isDropdownOpen;
+}
   
 }
