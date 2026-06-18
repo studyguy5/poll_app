8508d8ed44541d59adace2996cb0c8e2685dc979
Leonhard Siegele <leonhard.siegele39@gmail.com>
2026-06-18 12:37:45 +0200

    setUp FormControl for new Survey

--- src/app/components/create-survey-component/create-survey-component.ts
+++ src/app/components/create-survey-component/create-survey-component.ts
@@ -1,32 +1,33 @@
 import { Component, inject, Inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
+import { DOCUMENT, CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 
 @Component({
   selector: 'app-create-survey-component',
-  imports: [ReactiveFormsModule],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-survey-component.html',
-  styleUrl: './create-survey-component.scss',
+  styleUrls: ['./create-survey-component.scss'],
 })
 export class CreateSurveyComponent {
 
   document;
-  surveyName;
-  endDate;
-  category;
-  description;
-  questions;
+  surveyName: FormControl<string | null>  
+  endDate: FormControl<string | null>;
+  category: FormControl<string | null>;
+  description: FormControl<string | null>;
+  questions: FormArray<FormGroup>;
   letter: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   
   constructor(@Inject(DOCUMENT) document: Document) {
     this.document = document
-    this.surveyName = new FormControl('', Validators.required);
+    this.surveyName = new FormControl<string>('', {validators: [Validators.required, Validators.minLength(3)]});
     this.endDate = new FormControl('', Validators.required);
     this.category = new FormControl('');
-    this.description = new FormControl('');
-    this.questions = new FormArray([this.createQuestion()]);
+    this.description = new FormControl<string>('', {validators: [Validators.required, Validators.minLength(3)]});
+    this.questions = new FormArray([this.createQuestion()], {validators: [Validators.required, Validators.minLength(3)]}) as FormArray<FormGroup>;
  
   }
   ngOnInit() {
@@ -41,13 +42,20 @@ export class CreateSurveyComponent {
     this.questions.push(this.createQuestion());
   }
 
-  getAnswers(questionIndex: number): FormArray {
-  return this.questions.at(questionIndex).get('answers') as FormArray;
+  getAnswers(questionIndex: number): FormArray<FormControl<string | null>> {
+  return this.questions.at(questionIndex).get('answers') as FormArray<FormControl<string | null>>;
 }
 
   addAnswer(questionIndex: number) {
   this.getAnswers(questionIndex).push(this.createAnswer());
 }
+
+  deleteAnswer(questionIndex: number, answerIndex: number) {
+    const answers = this.getAnswers(questionIndex);
+    if (answerIndex >= 0 && answerIndex < answers.length) {
+      answers.removeAt(answerIndex);
+    }
+  }
   
 
   isDropdownOpen = false;
@@ -58,15 +66,15 @@ toggleDropDown() {
 }
 
 
-  createAnswer(): FormControl | FormGroup {
-  return new FormControl('');
+  createAnswer(): FormControl<string | null> {
+  return new FormControl<string>('');
 }
 
 createQuestion(): FormGroup {
   return new FormGroup({
-    question: new FormControl(''),
-    allowMultipleAnswers: new FormControl(false),
-    answers: new FormArray([
+    question: new FormControl<string>(''),
+    allowMultipleAnswers: new FormControl<boolean>(false),
+    answers: new FormArray<FormControl<string | null>>([
       this.createAnswer(),
       this.createAnswer()
     ])
